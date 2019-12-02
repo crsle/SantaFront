91b2419b77a346003044c11e4e603cbcf2c46c4e
Ysydra <bruna.seferian@gmail.com>
2019-12-02 16:38:44 +0100

    modif inscription

---
+++ src/app/inscription/inscription.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.css']
+})
+export class InscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+inscription(){
+
+}
+}

27d3c50369644d53443f01c32c71f596008a4e88
AubinM92 <aubin.mottier@gmail.com>
2019-12-05 16:27:09 +0100

    aide providentielle

--- src/app/mur/mur.component.ts
+++ src/app/mur/mur.component.ts
@@ -15,6 +15,9 @@ export class MurComponent implements OnInit {
 
   mursanta;
   parti;
+  test;
+
+  imgConv;
  p :Participation= new Participation();
   constructor(private myback: MybackService, private route: Router, private http: HttpClient,private dialog: MatDialog) {
 
@@ -37,7 +40,9 @@ export class MurComponent implements OnInit {
       this.http.get(this.myback.lienHTTP+'/mur/messages/'+ this.p.evenement.id)
       .subscribe(data2 => {
         this.mursanta = data2;
-        
+
+
+
 
       },err => {
         console.log(err);
@@ -50,10 +55,20 @@ export class MurComponent implements OnInit {
     });
     console.log(this.p);
 
+
+    this.http.get(this.myback.lienHTTP + 'mur/messages/2/7').subscribe( data => {
+      this.test = data;
+      this.imgConv = window.atob(this.test.image);
+    }, err => {
+      console.log(err);
+    })
+
 }
 
 
-
+changeImForm(img){
+  return window.atob(img);
+}
 
 openUploadImage() {
   const mydial =this.dialog.open(UploadimageComponent);

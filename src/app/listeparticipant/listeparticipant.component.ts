bb8fd47e1dab2743645cdf8330db6777257dd0c1
tmpdevfr <thibault.mp@sfr.fr>
2019-12-09 10:28:06 +0100

    bouton annuler tirage + corrections ergo

--- src/app/listeparticipant/listeparticipant.component.ts
+++ src/app/listeparticipant/listeparticipant.component.ts
@@ -8,6 +8,7 @@ import { PopupdetailparticipantComponent } from '../popupdetailparticipant/popup
 import { SSanta } from '../model/SSanta';
 import { Participation } from '../model/Participation';
 import { Button } from 'protractor';
+import { ConfirmationpopupComponent } from '../confirmationpopup/confirmationpopup.component';
 
 @Component({
   selector: 'app-listeparticipant',
@@ -118,7 +119,7 @@ export class ListeparticipantComponent implements OnInit {
   cloture(){
     this.http.post(this.myback.lienHTTP + '/clotureSanta' , this.myback.santa)
     .subscribe(data =>{
-      this.ngOnInit()
+      //this.ngOnInit()
     },err =>{
       console.log(err);
     });  
@@ -126,7 +127,10 @@ export class ListeparticipantComponent implements OnInit {
   }
 
   popupconfirmation(){
-    const mydial = this.dialog.open(PopupdetailparticipantComponent);
+    const mydial = this.dialog.open(ConfirmationpopupComponent);
+    this.ngOnInit();
+
   }
+
 
 }
